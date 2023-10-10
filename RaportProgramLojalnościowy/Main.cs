512ba7d368793bf14915f3b30a0e89e23e2a0b3a
SebastianFX2 <spietrowcow@fx2.pl>
2023-10-10 10:46:52 +0200

    Done

--- RaportProgramLojalnościowy/Main.cs
+++ RaportProgramLojalnościowy/Main.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using Soneta.Business;
+using Soneta.Business.UI;
+using Soneta.Handel;
+using Soneta.Types;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using CsvHelper;
+using Soneta.Core.DbTuples;
+using System.Runtime.InteropServices.ComTypes;
+using Soneta.Langs;
+
+[assembly: Worker(typeof(RaportProgramLojalnościowy.Main), typeof(DokHandlowe))]
 
 namespace RaportProgramLojalnościowy
 {
     public class Main
     {
+        public DokHandlowe dokHandlowe { get; set; }
+        [Context]
+        public Context context {  get; set; }
 
+
+        [Action(
+            "Export do CSV",
+            Priority = 1000,
+            Icon = ActionIcon.ExcelPrint,
+            Mode = ActionMode.Progress,
+            Target = ActionTarget.Menu | ActionTarget.ToolbarWithText)]
+        public NamedStream SaveDataWorker()
+        {
+ 
+            string filename = context.Login.Database.Name + "_" + Date.Today+".csv";
+            var nowyPlik = new StreamWriter(new MemoryStream(), Encoding.UTF8);
+
+            WriteCSV writeCsv = new WriteCSV();
+            writeCsv.WriteHeader(nowyPlik);
+            writeCsv.GetData(nowyPlik,context);
+            nowyPlik.Flush();  
+
+           return new NamedStream(filename, ((System.IO.MemoryStream)nowyPlik.BaseStream).ToArray());
+
+        }
+
+
+        public static bool IsVisibleFun(Context cx)
+        {
+            UILocation uILocation = cx[typeof(UILocation)] as UILocation;
+            return uILocation.FolderNormalizedPath == "Handel/Sprzedaz/FakturySprzedazy";
+        }
     }
 }
---
+++ RaportProgramLojalnościowy/WriteCSV.cs
@@ -0,0 +1,58 @@
+﻿using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaportProgramLojalnościowy
+{
+    internal class WriteCSV
+    {
+        public void WriteHeader(System.IO.StreamWriter plik)
+        {
+            plik.WriteLine("NIP;Numer klienta LS;Nr dok. Sprz.;Data sprzed.;Symbol (kod);" +
+                              "Ilość;Ilość punktów za towar;Suma punktów za towar");
+        }
+
+        public void GetData(System.IO.StreamWriter plik, Context context)
+        {
+            View nav = context[typeof(View)] as View;
+            HandelModule hm = HandelModule.GetInstance(context.Session);
+            string[] wiersz = new string[8];
+
+
+            foreach (DokumentHandlowy dh in nav)
+            {
+                if(!dh.Definicja.Symbol.Contains("FV"))
+                {
+                    continue;
+                }
+
+                foreach(PozycjaDokHandlowego poz in dh.Pozycje)
+                {
+                    //bool IsNumerKlientaLs = dh.Kontrahent.Features["Numer klient LS"] != null;
+
+                    wiersz[0] = dh.Kontrahent.NIP;//NIP
+                    wiersz[1] = dh.Kontrahent.Features["Numer klienta LS"].ToString();//Numer klienta LS
+                    wiersz[2] = dh.NumerPelnyZapisany;//Nr dokumentu sprzedaży
+                    wiersz[3] = dh.Data.ToString();//Data sprzedaży
+                    wiersz[4] = poz.Symbol.ToString();//symbol(kod) towaru/pozycji
+                    wiersz[5] = poz.Ilosc.ToString();//ilosc
+                    wiersz[6] = poz.Towar.Features["Punkty lojalnościowe"].ToString();//ilosc punktów za towar
+                    wiersz[7] = (poz.Ilosc.Value * Int32.Parse(poz.Towar.Features["Punkty lojalnościowe"].ToString())).ToString();
+
+                    StringBuilder builder = new StringBuilder();
+
+                    foreach(string wartosc in wiersz)
+                    {
+                        builder.Append(wartosc);
+                        builder.Append(';');
+                    }
+                    plik.WriteLine(builder.ToString());
+                }
+
+
+            }
+        }
+    }
+}
